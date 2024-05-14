e33527bce7ee1b8b88ffc1b21ca132131d4fb1fc
HazelDaniel <hazeldaniel856@gmail.com>
2024-05-14 15:54:51 +0100

    task-2: re-submission

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ import { MountEndpoints } from './routes';
 
 const app = express();
 
+/**
+ * Represents an error in this API.
+ */
+export class APIError extends Error {
+  constructor(code, message) {
+    super();
+    this.code = code || 500;
+    this.message = message;
+  }
+}
+
+/**
+ * This middleware generate a custom error response
+ * @param {Error} err The error object.
+ * @param {Request} req The Express request object.
+ * @param {Response} res The Express response object.
+ * @param next The Express next function.
+ */
+export const errorResponse = (err, req, res, next) => {
+  const defaultMsg = `Failed to process ${req.url}`;
+
+  if (err instanceof APIError) {
+    res.status(err.code).json({ error: err.message || defaultMsg });
+    return;
+  }
+  return res.status(500).json({
+    error: err ? err.message || err.toString() : defaultMsg,
+  });
+};
+
+
 const startServer = (api) => {
   const port = process.env.PORT || 5000;
   const env = process.env.npm_lifecycle_event || 'dev';
