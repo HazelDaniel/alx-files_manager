6753f86565f65d9a21c90a056e540fa332862c66
HazelDaniel <hazeldaniel856@gmail.com>
2024-05-14 16:11:41 +0100

    task-3: submission

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
 
 export function MountEndpoints (api) {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+	api.post('/users', UsersController.postNew);
 }
